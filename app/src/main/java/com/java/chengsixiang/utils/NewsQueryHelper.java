0eb54d9da28bebb83bc9ed08aab48777cd7fff69
程思翔 <2021010761@secoder.net>
2023-08-30 18:41:55 +0800

    fix main thread network operation

--- app/src/main/java/com/java/chengsixiang/utils/NewsQueryHelper.java
+++ app/src/main/java/com/java/chengsixiang/utils/NewsQueryHelper.java
@@ -1,5 +1,7 @@
 package com.java.chengsixiang.utils;
 
+import android.util.Log;
+
 import java.io.IOException;
 
 import okhttp3.Callback;
@@ -10,19 +12,22 @@ import okhttp3.Response;
 public class NewsQueryHelper {
     private final OkHttpClient client = new OkHttpClient();
 
-    public NewsItem[] queryNews(String size, String startDate, String endDate, String words, String categories, Callback callback) {
+    public void queryNews(String size, String startDate, String endDate, String words, String categories) {
         String apiUrl = String.format("https://api2.newsminer.net/svc/news/queryNewsList?size=%s&startDate=%s&endDate=%s&words=%s&categories=%s", size, startDate, endDate, words, categories);
+        Log.d("NewsQueryHelper", apiUrl);
         Request request = new Request.Builder().url(apiUrl).build();
         try {
             Response response = client.newCall(request).execute();
             if (response.isSuccessful()) {
                 String resData = response.body().string();
+                Log.d("NewsQueryHelper", resData);
                 // you may try obj =  Gson().fromJson(responseData, yourClass.class);
-                return null;
+            }
+            else {
+                Log.d("NewsQueryHelper", "Failed to get news data");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
