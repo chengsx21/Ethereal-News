3c4397b1e5caea7aba675ef0083ace01c693e399
程思翔 <chengsx_thu@163.com>
2023-08-30 18:41:55 +0800

    fix main thread network operation

--- app/src/main/java/com/java/chengsixiang/SearchResultActivity.java
+++ app/src/main/java/com/java/chengsixiang/SearchResultActivity.java
@@ -1,13 +1,50 @@
 package com.java.chengsixiang;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.java.chengsixiang.utils.NewsQueryHelper;
+
+import java.util.Objects;
+
 public class SearchResultActivity extends AppCompatActivity {
+    private String defaultSize = "20";
+    private String words;
+    private String startDate;
+    private String endDate;
+    private String categories;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_list);
+
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText("Search Results");
+        Bundle bundle = this.getIntent().getExtras();
+        assert bundle != null;
+        words = bundle.getString("words");
+        startDate = bundle.getString("startDate");
+        endDate = bundle.getString("endDate");
+        categories = bundle.getString("categories");
+        if (Objects.equals(startDate, "默认向前检索所有新闻")) {
+            startDate = "";
+        }
+        if (Objects.equals(endDate, "默认为当前时间")) {
+            endDate = "";
+        }
+        if (Objects.equals(categories, "全部")) {
+            categories = "";
+        }
+        NewsQueryHelper newsHelper = new NewsQueryHelper();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                newsHelper.queryNews(defaultSize, startDate, endDate, words, categories);
+            }
+        }).start();
     }
 }
