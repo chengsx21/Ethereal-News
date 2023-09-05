a3669ddf15f33ac3950ef693b4b0c7f153429631
程思翔 <2021010761@secoder.net>
2023-09-05 22:12:08 +0800

    fix code style

--- app/src/main/java/com/java/chengsixiang/Activity/CategorySetting.java
+++ app/src/main/java/com/java/chengsixiang/Activity/CategorySetting.java
@@ -35,7 +35,7 @@ public class CategorySetting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category_list);
+        setContentView(R.layout.category_setting);
         context = this;
         setCategories();
         setBackButton();
--- app/src/main/java/com/java/chengsixiang/Activity/HomePage.java
+++ app/src/main/java/com/java/chengsixiang/Activity/HomePage.java
@@ -18,7 +18,7 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.home_page);
         setTabLayout();
         setSettingButton();
         setFavoriteButton();
